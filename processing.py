d784eaa9015c663ddb49a8a726002567096706e1
Henok Gizaw <henokgizaw06@gmail.com>
2025-06-12 14:15:46 +0300

    Add files via upload

--- processing.py
+++ processing.py
@@ -1,47 +1,55 @@
-
 import fitz
+import os
 import nltk
 import re
-import os
-def read_pdf_text(path):
-    with fitz.open("document.pdf") as doc:
-        text = ""
-        for page in doc:
-            text += page.get_text()
-    return text
-def stopword_removal(words):
-    stopwords = [
-        "እኔ", "አንቺ", "አንተ", "እሱ", "እሷ", "እኛ", "እናንተ", "እነሱ",
-        "ይህ", "ያ", "የዚህ", "የዛ", "ይሄ", "እነዚህ", "እነዚያ",
-        "እንደ", "ወደ", "ከ", "በ", "ላይ", "በታች", "በኩል",
-        "ያለ", "ነው", "ና", "ነች", "ናት", "ነን", "ናቸው",
-        "ማን", "ምን", "መቼ", "የት", "እንዴት", "ለምን",
-        "እንዲሁ", "ከዚያ", "ከዛ", "እስከ", "ስለ", "ስለምን",
-        "እንኳን", "ደግሞ", "እየ", "እንደኛ", "እንዲሁም", "ሁሉ", "ሁሉም",
-        "ያን", "እርሱ", "እርሷ", "ያች", "ያንን", "አሁን", "በዚህ",
-        "ያም", "እኔም", "እርስዎ", "ያህል", "እዚያ", "በዚያ", "በዚህም"
-    ]
-    newword=list() # storing non - stop words
-    count=0
-    knownstopwords = list() # storing known stop words
-    for w in words :
-        if w not in stopwords:
-            newword.append(w)
-            count += 1
-        else:
-            knownstopwords.append(w)
-    return newword
-def normalization(word):
-    text = re.sub(r'[።፣፤፥፦፧""]', '', word)
-    text = re.sub(r'\s+', ' ', word).strip()
-    return text
-def tokenization(word):
-        # using re module the program can split
-    parts = re.split(r'[]', word)# add amharic words
-    clean=[s.strip() for s in parts if s.strip()]
-    return clean
-
-
-document = input("Give me a document as path: ")
-String = read_pdf_text(document)
-print(String)
+# check if it's working good
+class file_reading:
+    @staticmethod
+    def read_pdf_text(path):
+        with fitz.open(path) as doc:
+            text = ""
+            for page in doc:
+                text += page.get_text()
+        return text
+    # working good
+    @staticmethod
+    def stopword_removal(tokens):
+        stopwords = [
+            "እኔ", "አንቺ", "አንተ", "እሱ", "እሷ", "እኛ", "እናንተ", "እነሱ",
+            "ይህ", "ያ", "የዚህ", "የዛ", "ይሄ", "እነዚህ", "እነዚያ",
+            "እንደ", "ወደ", "ከ", "በ", "ላይ", "በታች", "በኩል",
+            "ያለ", "ነው", "ና", "ነች", "ናት", "ነን", "ናቸው",
+            "ማን", "ምን", "መቼ", "የት", "እንዴት", "ለምን",
+            "እንዲሁ", "ከዚያ", "ከዛ", "እስከ", "ስለ", "ስለምን",
+            "እንኳን", "ደግሞ", "እየ", "እንደኛ", "እንዲሁም", "ሁሉ", "ሁሉም",
+            "ያን", "እርሱ", "እርሷ", "ያች", "ያንን", "አሁን", "በዚህ",
+            "ያም", "እኔም", "እርስዎ", "ያህል", "እዚያ", "በዚያ", "በዚህም"
+        ]
+        newword=list() # storing non - stop words
+        count=0
+        knownstopwords = list() # storing known stop words
+        for w in tokens :
+            if w not in stopwords:
+                newword.append(w)
+            else:
+                knownstopwords.append(w)
+        return [knownstopwords,newword]
+    # removing the  amharic quotations
+    #normalization is working good
+    @staticmethod
+    def normalization(word):
+        punctuations ='።፣፤፥፦፧"""()!'
+        newstring = str()
+        word_punctuations = str()
+        # removing the quotations
+        for i in word:
+            if i not in punctuations:
+                newstring += i
+            else:
+                newstring += ""
+                word_punctuations += i
+        return [newstring,word_punctuations]
+    # tokenization of the string is working good
+    @staticmethod
+    def tokenization(word):
+        return word.split()
